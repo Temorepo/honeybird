2f3656c7afaedfb86691afe09b1afac709e266f0
charlie.groves <charlie.groves@41498284-90ee-11de-b664-3b115b7b7a9b>
2009-08-25 07:12:19 +0000

    Add enough tests on filter creation that I'm not going to completely destroy them.

---
+++ trunk/test/java/com/threerings/honeybird/MetricTest.java
@@ -0,0 +1,26 @@
+package com.threerings.honeybird;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MetricTest
+    implements Metrics, Dimensions
+{
+    @Test
+    public void testOrWithMetric ()
+    {
+        assertEquals("ga:pageviews>1000,ga:pagePath=@/honey",
+            PAGEVIEWS.gt("1000").or(PAGE_PATH.contains("/honey")).makeFilter());
+    }
+
+    @Test
+    public void testOrNestings ()
+    {
+        String expected = "ga:pageviews>1000,ga:bounces<100,ga:exits>=10";
+        assertEquals(expected,
+            PAGEVIEWS.gt("1000").or(BOUNCES.lt("100").or(EXITS.gte("10"))).makeFilter());
+        assertEquals(expected,
+            PAGEVIEWS.gt("1000").or(BOUNCES.lt("100")).or(EXITS.gte("10")).makeFilter());
+    }
+}
