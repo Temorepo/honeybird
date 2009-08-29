658bdc63cedaf50dff788055770996f007148f1a
charlie.groves <charlie.groves@41498284-90ee-11de-b664-3b115b7b7a9b>
2009-08-29 04:49:49 +0000

    Allow sort and descending sort parameters to be added

--- src/java/com/threerings/honeybird/QueryBuilder.java
+++ src/java/com/threerings/honeybird/QueryBuilder.java
@@ -23,13 +23,15 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.gdata.client.analytics.AnalyticsService;
 import com.google.gdata.client.analytics.DataQuery;
 import com.google.gdata.data.analytics.DataFeed;
-import com.google.gdata.util.common.base.Joiner;
+import com.google.gdata.util.common.base.Join;
 
 /**
  * Constructs queries using the google analytics API and conveniently exposes their results.
@@ -84,6 +86,39 @@ public class QueryBuilder
     }
 
     /**
+     * Sorts the results from this query in ascending order by the given sources, in the order
+     * that the sources are given. If the query already contains sort sources, these sources are
+     * applied after the existing sources.
+     */
+    public QueryBuilder sort (Source<?>... sources)
+    {
+        for (Source<?> source : sources) {
+            if (!_sorts.contains(source.getName())) {
+                _sorts.add(source.getName());
+                addSource(source);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Sorts the results from this query in descending order by the given sources, in the order
+     * that the sources are given. If the query already contains sort sources, these sources are
+     * applied after the existing sources.
+     */
+    public QueryBuilder sortDescending (Source<?>... sources)
+    {
+        for (Source<?> source : sources) {
+            String sort = "-" + source.getName();
+            if (!_sorts.contains(sort)) {
+                _sorts.add(sort);
+                addSource(source);
+            }
+        }
+        return this;
+    }
+
+    /**
      * Sets the start day for the query.
      */
     public QueryBuilder from (Date date)
@@ -128,8 +163,9 @@ public class QueryBuilder
         if (_filter != null) {
             _query.setFilters(_filter.makeFilter());
         }
-        _query.setMetrics(Joiner.on(",").join(_metrics));
-        _query.setDimensions(Joiner.on(",").join(_dimensions));
+        _query.setMetrics(Join.join(",", _metrics));
+        _query.setDimensions(Join.join(",", _dimensions));
+        _query.setSort(Join.join(",", _sorts));
         DateFormat queryDayFormat = new SimpleDateFormat("yyyy-MM-dd");
         _query.setStartDate(queryDayFormat.format(_start));
         _query.setEndDate(queryDayFormat.format(_end));
@@ -153,6 +189,9 @@ public class QueryBuilder
     protected final Set<String> _dimensions = Sets.newHashSet();
 
     protected final Set<String> _metrics = Sets.newHashSet();
+
+    // Store sort sources in a List as their order matters
+    protected final List<String> _sorts = Lists.newArrayList();
 
     protected final AnalyticsService _service;
 }
